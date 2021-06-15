10c79cb49090faf2b591f85ca43c9862c02d2a9f
Sabbaghh <afsabbagh9@gmail.com>
2021-06-15 08:48:16 +0300

    Getting products data from database && refactoring server.js code

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,24 @@
-import express from 'express'
-import products from './data/products.js'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import productRoutes from './routes/productRoute.js';
 
-connectDB()
-dotenv.config()
-const app = express()
+connectDB();
+dotenv.config();
+const app = express();
 app.get('/', (req, res) => {
 	res.json({
 		name: 'abdallah',
 		lastName: 'sabbagh',
-	})
-})
+	});
+});
 
-app.get('/api/products', (req, res) => {
-	res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((pro) => pro._id === req.params.id)
-	res.json(product)
-})
-
-const PORT = process.env.PORT || 5000
+app.use('/api/products', productRoutes);
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(
 		`server running in ${process.env.NODE_ENV} on Port ${process.env.PORT}`
 			.yellow.underline.bold,
-	)
-})
+	);
+});
