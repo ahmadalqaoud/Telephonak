064164f60437aae6fa434ee4ae4cca1bdbbff79f
Sabbaghh <afsabbagh9@gmail.com>
2021-06-16 08:09:18 +0300

    Wrapping screens with custom loadErrHandler component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { getProducts } from '../redux/actions/productsActions';
+import LoadingErrHandler from '../components/LoadErrHandler';
 
 //redux
 import { useSelector, useDispatch } from 'react-redux';
 const HomeScreen = () => {
 	const dispatch = useDispatch();
 	const { products, loading, error } = useSelector((state) => state.products);
-	// const [products, setProducts] = useState([]);
-
 	useEffect(() => {
 		dispatch(getProducts());
 	}, [dispatch]);
 	return (
 		<>
-			<h1>LATEST </h1>
-			<Row>
-				{products && products.length > 0 ? (
-					<>
-						{products.map((product) => {
-							return (
-								<Col key={product._id} sm={12} md={4} lg={4} xl={3}>
-									<Product product={product} />
-								</Col>
-							);
-						})}
-					</>
-				) : (
-					<h6>There are no products for now!</h6>
-				)}
-			</Row>
+			<LoadingErrHandler
+				loading={loading}
+				error={error ? 'OOPS! Something went wrong!' : false}
+			>
+				<h1>LATEST </h1>
+				<Row>
+					{products && products.length > 0 ? (
+						<>
+							{products.map((product) => {
+								return (
+									<Col key={product._id} sm={12} md={4} lg={4} xl={3}>
+										<Product product={product} />
+									</Col>
+								);
+							})}
+						</>
+					) : (
+						<h6>There are no products for now!</h6>
+					)}
+				</Row>
+			</LoadingErrHandler>
 		</>
 	);
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Button, ListGroup, Card, Image } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import axios from 'axios';
+import LoadErrHandler from '../components/LoadErrHandler';
 const ProductScreen = ({ match }) => {
 	const [product, setProduct] = useState();
 	useEffect(() => {
@@ -18,70 +19,72 @@ const ProductScreen = ({ match }) => {
 	}, [match.params.id]);
 	return (
 		<>
-			{product ? (
-				<>
-					<Link className='btn btn-dark my-3' to='/'>
-						GO BACK
-					</Link>
-					<Row>
-						<Col lg={6} md={6}>
-							<Image src={product.image} alt={product.name} fluid />
-						</Col>
-						<Col lg={6} md={6}>
-							<ListGroup variant='flush'>
-								<ListGroup.Item>
-									<h4>{product?.name?.toUpperCase()}</h4>
-								</ListGroup.Item>
-								<ListGroup.Item>
-									<Rating
-										rating={product.rating}
-										text={`${product.numReviews} reviews`}
-									/>
-								</ListGroup.Item>
-								<ListGroup.Item>
-									<h3>PRICE : ${product.price}</h3>
-								</ListGroup.Item>
-								<ListGroup.Item>
-									<h5>DESCRIPTION :</h5>
-									<p>{product.description}</p>
-								</ListGroup.Item>
-							</ListGroup>
-						</Col>
-						<Col className='my-4' md={{ span: 6, offset: 3 }}>
-							<Card>
+			<LoadErrHandler loading={false} error={`OOPS! something went wrong`}>
+				{product && (
+					<>
+						<Link className='my-3 btn btn-dark' to='/'>
+							GO BACK
+						</Link>
+						<Row>
+							<Col lg={6} md={6}>
+								<Image src={product.image} alt={product.name} fluid />
+							</Col>
+							<Col lg={6} md={6}>
 								<ListGroup variant='flush'>
 									<ListGroup.Item>
-										<Row>
-											<Col>PRICE :</Col>
-											<Col>{product.price}</Col>
-										</Row>
+										<h4>{product?.name?.toUpperCase()}</h4>
 									</ListGroup.Item>
 									<ListGroup.Item>
-										<Row>
-											<Col> STATUS :</Col>
-											<Col>
-												{product.countInStock > 0 ? 'In stock' : 'Out of stock'}
-											</Col>
-										</Row>
+										<Rating
+											rating={product.rating}
+											text={`${product.numReviews} reviews`}
+										/>
 									</ListGroup.Item>
 									<ListGroup.Item>
-										<Button
-											className='btn btn-block btn-dark'
-											type='button'
-											style={{ width: '100%' }}
-											disabled={product.countInStock <= 0}
-										>
-											ADD TO CART
-										</Button>
+										<h3>PRICE : ${product.price}</h3>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<h5>DESCRIPTION :</h5>
+										<p>{product.description}</p>
 									</ListGroup.Item>
 								</ListGroup>
-							</Card>
-						</Col>
-					</Row>
-				</>
-			) : (
-				<h6 className='text-dark'>product not found</h6>
-			)}
+							</Col>
+							<Col className='my-4' md={{ span: 6, offset: 3 }}>
+								<Card>
+									<ListGroup variant='flush'>
+										<ListGroup.Item>
+											<Row>
+												<Col>PRICE :</Col>
+												<Col>{product.price}</Col>
+											</Row>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Row>
+												<Col> STATUS :</Col>
+												<Col>
+													{product.countInStock > 0
+														? 'In stock'
+														: 'Out of stock'}
+												</Col>
+											</Row>
+										</ListGroup.Item>
+										<ListGroup.Item>
+											<Button
+												className='btn btn-block btn-dark'
+												type='button'
+												style={{ width: '100%' }}
+												disabled={product.countInStock <= 0}
+											>
+												ADD TO CART
+											</Button>
+										</ListGroup.Item>
+									</ListGroup>
+								</Card>
+							</Col>
+						</Row>
+					</>
+				)}
+			</LoadErrHandler>
 		</>
 	);
 };
