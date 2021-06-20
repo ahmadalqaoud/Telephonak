48f1971a49ec52f11383da4edd43f134b81422b4
Sabbaghh <afsabbagh9@gmail.com>
2021-06-20 23:22:40 +0300

    Initialize register router

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,36 @@ import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
 import generateAuthToken from '../utilities/generateToken.js';
 
+//@des      Register a new user
+//@route    POST '/api/users'
+//@access   Public
+export const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+	//if user exist
+	const userExist = await User.findOne({ email });
+	if (userExist) {
+		res.status(400);
+		throw new Error('user already exist');
+	}
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateAuthToken(user._id),
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
+});
+
 //@des      auth user & get token
 //@route    POST '/api/users/login'
 //@access   Public
