07cfb47540fcdf73cab3eaf294cfea8212ff4d1f
Sabbaghh <afsabbagh9@gmail.com>
2021-07-10 04:44:05 +0300

    Implementing get ordersById and getAllOrdersByUserId endpoints

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,38 @@ export const addOrderItems = asyncHandler(async (req, res) => {
 		res.status(201).json(createdOrder);
 	}
 });
+
+// @des     GET ORDER BY ID
+//@route    GET '/api/orders/:id'
+//@access   Private
+
+export const getOrderById = asyncHandler(async (req, res) => {
+	const id = req.params.id;
+	const order = await Order.findById(id).populate('user', 'name email');
+	if (!order) {
+		res.status(404);
+		throw new Error('no order found');
+		return;
+	}
+	if (req.user._id.toString() === order.user._id.toString()) {
+		res.json(order);
+	} else {
+		res.status(401);
+		throw new Error('user not authorized');
+	}
+});
+// @des     GET ORDERS BY USER ID
+//@route    GET '/api/orders'
+//@access   Private
+export const getAllOrdersByUserID = asyncHandler(async (req, res) => {
+	const order = await Order.find({ user: req.user._id }).populate(
+		'user',
+		'name email',
+	);
+	if (!order) {
+		res.status(404);
+		throw new Error('no order found');
+		return;
+	}
+	res.json(order);
+});
