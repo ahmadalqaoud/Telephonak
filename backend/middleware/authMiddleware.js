10e6a6166a8eff3a32154fa36c4ea1cf5d7efd29
Sabbaghh <afsabbagh9@gmail.com>
2021-07-15 11:38:19 +0300

    Initialize admin MiddleWare

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -21,3 +21,12 @@ export const protect = asyncHandler(async (req, res, next) => {
 	}
 	next();
 });
+
+export const admin = asyncHandler(async (req, res, next) => {
+	if (req.user && req.user.isAdmin) {
+		next();
+	} else {
+		res.status(401);
+		throw new Error('not authorized as admin');
+	}
+});
