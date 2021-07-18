642167e706b69c5d7e590a0177fee624b3b80829
Sabbaghh <afsabbagh9@gmail.com>
2021-07-18 05:47:06 +0300

    Implementing deleteHandler within usersListScreen

--- frontend/src/screens/UsersListScreen.js
+++ frontend/src/screens/UsersListScreen.js
@@ -1,22 +1,32 @@
 import React, { useEffect } from 'react';
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, Alert } from 'react-bootstrap';
 import LoadErrHandler from '../components/LoadErrHandler';
 import { LinkContainer } from 'react-router-bootstrap';
 //redux
 import { useSelector, useDispatch } from 'react-redux';
-import { getUsers } from '../redux/actions/userActions';
+import { getUsers, deleteUser } from '../redux/actions/userActions';
 
 const UsersListScreen = ({ history }) => {
 	const dispatch = useDispatch();
 	const { loading, error, users } = useSelector((state) => state.usersList);
 	const { userInfo } = useSelector((state) => state.userLogin);
+	const {
+		loading: userDeleteLoading,
+		success: userDeleteSuccess,
+		error: userDeleteError,
+	} = useSelector((state) => state.userDelete);
+	const deleteHandler = (id) => {
+		if (window.confirm('Are you sure you want to delete this user?')) {
+			dispatch(deleteUser(id));
+		}
+	};
 	useEffect(() => {
-		if (userInfo && userInfo.isAdmin) {
+		if ((userInfo && userInfo.isAdmin) || userDeleteSuccess) {
 			dispatch(getUsers());
 		} else {
 			history.push('/');
 		}
-	}, [dispatch, userInfo, history]);
+	}, [dispatch, userInfo, history, userDeleteSuccess]);
 
 	return (
 		<LoadErrHandler
@@ -24,6 +34,12 @@ const UsersListScreen = ({ history }) => {
 			error={error ? 'Ops! something went wrong' : ''}
 		>
 			<h1>users</h1>
+			{userDeleteLoading && <h5>loading...</h5>}
+			{userDeleteError && (
+				<Alert className='full-width' variant='danger'>
+					{userDeleteError}
+				</Alert>
+			)}
 			{users ? (
 				<Table striped bordered hovered responsive className='table-sm'>
 					<thead>
@@ -49,7 +65,13 @@ const UsersListScreen = ({ history }) => {
 												EDIT
 											</Button>
 										</LinkContainer>
-										<Button variant='danger' className='btn-sm'>
+										<Button
+											variant='danger'
+											className='btn-sm'
+											onClick={() => {
+												deleteHandler(user._id);
+											}}
+										>
 											DELETE
 										</Button>
 									</td>
