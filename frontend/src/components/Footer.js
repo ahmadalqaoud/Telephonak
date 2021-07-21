18845f388484be9bb097e202a38f78297390cd45
Sabbaghh <afsabbagh9@gmail.com>
2021-07-21 19:56:37 +0300

    Removing footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 const Footer = () => {
 	return (
 		<footer>
-			<Container>
+			{/* <Container>
 				<Row>
 					<Col className='text-center py-3'>
-						<span>Handcrafted by me © SABBAGH</span>
+						<span>All copyrights reserved</span>
 					</Col>
 				</Row>
-			</Container>
+			</Container> */}
 		</footer>
-	)
-}
+	);
+};
 
-export default Footer
+export default Footer;
